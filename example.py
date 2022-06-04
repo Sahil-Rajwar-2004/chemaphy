4d2d305677dfc182eba593315d17f2d560505216
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-06-04 14:33:37 +0530

    update example.py
    including latest version

--- example.py
+++ example.py
@@ -1,6 +1,15 @@
 import chemaphy
 from chemaphy import ProjectileMotion as pm
 from chemaphy import LogicGates as lg
+from chemaphy import AlternatingCurrent as sc
 
 print(pm.horizontal_range(100,chemaphy.g_Earth.value,45))
+print(ac.vrms2v(220))
 print(lg.OR(1,0))
+
+"""
+Outputs:
+1020.41 m
+311.13 Volts
+True
+"""
