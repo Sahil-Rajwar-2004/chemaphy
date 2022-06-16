e3f5fe66eed7f9ae2b5c68ffcc81e8d4f0e18bbd
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-06-16 22:15:55 +0530

    Update example.py
    v-0.7.4

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ from chemaphy import LogicGates as lg
 from chemaphy import AlternatingCurrent as ac
 from chemaphy import Statistics as stats
 
-print(pm.horizontal_range(100,chemaphy.g_Earth.value,45))
-print(ac.vrms2v(220))
+print(pm.(100,chemaphy.g_earth.value,45))
+print(ac.Vrms2V(220))
 print(lg.OR(1,0))
 print(stats.Mean([1,2,3,4,5,6,7,8,9,0]))
 print(stats.Median([1,2,3,4,5,6,7,8,9,0]))
