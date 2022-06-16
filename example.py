023ed2d3828c440b4225ac9a90b3fde37b81acf0
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2022-06-16 22:13:19 +0530

    Update example.py
    v-0.7.4

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import chemaphy
 from chemaphy import ProjectileMotion as pm
 from chemaphy import LogicGates as lg
-from chemaphy import AlternatingCurrent as sc
+from chemaphy import AlternatingCurrent as ac
 from chemaphy import Statistics as stats
 
 print(pm.horizontal_range(100,chemaphy.g_Earth.value,45))
@@ -9,12 +9,4 @@ print(ac.vrms2v(220))
 print(lg.OR(1,0))
 print(stats.Mean([1,2,3,4,5,6,7,8,9,0]))
 print(stats.Median([1,2,3,4,5,6,7,8,9,0]))
-
-"""
-Outputs:
-1020.41 m
-311.13 Volts
-True
-4.5
-5.5
-"""
+print(stats.Quartiles([375, 211, 23, 39, 118, 97, 971, 702, 143, 9]))
